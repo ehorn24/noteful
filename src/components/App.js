b057c4a84e23e8bdc977768e99785bbbde43d977
Ella Horn <ellalouisahorn@yahoo.com>
2019-11-11 17:49:06 -0500

    added routes, store, header, made main & sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,49 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Main from "./Main";
+import STORE from "./components/store";
 
 export default class App extends Component {
+  state = {
+    folders: [...STORE.folders],
+    notes: [...STORE.notes]
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <Router>
+        <Header />
+        <Route
+          path="/"
+          exact={true}
+          render={props => (
+            <>
+              <Sidebar {...props} />
+              <Main {...props} />
+            </>
+          )}
+        />
+        <Route
+          path="/folder/:folderId"
+          render={props => (
+            <>
+              <Sidebar {...props} />
+              <Main {...props} />
+            </>
+          )}
+        />
+        <Route
+          path="/note/:noteId"
+          render={props => (
+            <>
+              <Sidebar {...props} />
+              <Main {...props} />
+            </>
+          )}
+        />
+      </Router>
+    );
   }
 }
