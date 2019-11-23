a98855e8de833ecb7da702d6c573653ed00bd41e
Ella Horn <ellalouisahorn@yahoo.com>
2019-11-23 13:21:18 -0500

    added note scss

--- src/components/Note.js
+++ src/components/Note.js
@@ -4,8 +4,22 @@ const Note = ({ match, notes }) => {
   let currentNote = notes.filter(note => note.id === match.params.noteId);
   return (
     <main>
-      <h3>Note Content</h3>
-      {currentNote[0].content}
+      <div className="note-header">
+        <h3>{currentNote[0].name}</h3>
+        <time>
+          {new Date(currentNote[0].modified).toLocaleDateString("en-US", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true
+          })}
+        </time>
+        <button>Delete</button>
+      </div>
+      <p>{currentNote[0].content}</p>
     </main>
   );
 };
