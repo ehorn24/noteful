7bb6d3490e85f1d64d4d3c03c159bb6657fea137
Ella Horn <ellalouisahorn@yahoo.com>
2020-02-08 15:02:09 -0500

    removed xss

--- server/folders/folders-router.js
+++ server/folders/folders-router.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const express = require("express");
-const xss = require("xss");
 const FoldersService = require("./folders-service");
 
 const foldersRouter = express.Router();
